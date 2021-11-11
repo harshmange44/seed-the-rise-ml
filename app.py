ace2052e8d7377f3a0391ff6446bc5a115278b1d
Harsh Mange <harshmange44@gmail.com>
2021-11-11 13:52:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ def predict():
 
         # Get predictions
         prediction = predict_health(temperature, humidity, str(cropname))
-        response = make_response(str(prediction), 200)
-        response.mimetype = "text/plain"
+        response = make_response(jsonify(prediction), 200)
+        response.mimetype = "application/json"
         return response
 
 if __name__ == '__main__':
